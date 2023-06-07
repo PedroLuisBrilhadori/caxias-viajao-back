86b5d2f80d154c8ddcb3a143938b4e53a9fd1ecf
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2023-06-07 11:50:44 -0300

    feat: add custom name in argv on generate-cache

--- generate-cache.py
+++ generate-cache.py
@@ -5,22 +5,29 @@ from utils.files import read_file
 
 
 def main(): 
-    if len(argv) != 2:
+    if len(argv) == 1:
         return print('Command wrong. Please, try again. \n\nExample: \npython generate-cache.py ./filepath')
     
+    custom_name = ''
+    if len(argv) == 3: 
+        custom_name = argv.pop()
+
     file_path = argv.pop()
 
     if exists(file_path): 
-        return generate_cache(file_path)
+        return generate_cache(file_path, custom_name)
     
     return print(f'the file: {file_path} not exists')
     
 
 
-def generate_cache(file_path): 
+def generate_cache(file_path, custom_name): 
     cache = False 
-    name = basename(file_path).split('.')[0]
     data = ''
+    name = basename(file_path).split('.')[0]
+    
+    if len(custom_name) > 0: 
+        name = custom_name
 
     try: 
         data = read_file(file_path)
